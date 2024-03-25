ed97889267465887ea862b7db30248111e8f0a79
Mike Coletta <coletta.mike@gmail.com>
2024-03-25 16:36:05 -0700

    Added score and rewards

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import kaboom from "kaboom"
 
+// keep track of score
+let score = 0;
+
 const k = kaboom({
 	global: true,
 	stretch: true, // Resize the content
@@ -44,9 +47,39 @@ function spawnObstacle() {
 	wait(rand(0.5, 1.5), spawnObstacle);
 }
 
-spawnObstacle()
+function spawnReward() {
+	k.add([
+		k.rect(20, 20),
+		k.pos(rand(10, 470), -20),
+		k.area(),
+		k.outline(4),
+		k.color(20, 150, 15),
+		k.move(DOWN, player.speed),
+		"reward",
+	])
+
+	// wait a random amount of time to spawn next tree
+	wait(rand(2.5, 3.5), spawnReward);
+}
+
+spawnObstacle();
+spawnReward();
 
 player.onCollide("obstacle", () => {
 	k.addKaboom(player.pos);
 	k.shake();
+});
+
+player.onCollide("reward", () => {
+	score += 10;
+	k.shake();
+});
+
+const scoreLabel = add([
+	k.text(score),
+	k.pos(24, 24),
+]);
+
+onUpdate(() => {
+	scoreLabel.text = score;
 });
